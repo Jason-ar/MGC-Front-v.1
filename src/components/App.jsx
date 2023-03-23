1ad323d5a54724f33fc59b170e0d5aa199c5ed27
Jason notebook <jrparedesonline@gmail.com>
2023-03-22 21:02:47 -0300

    prueba 1

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () =>
+
+    <div class="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Webpack!</h1>
+        <button class="btn btn-primary">Primary button</button>
+    </div>
+
+
+export default App;
